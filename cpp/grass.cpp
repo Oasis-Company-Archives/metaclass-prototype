d015115c68d2a7eea532a2defa3f2e74ad0e7aa3
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-04 22:21:19 +0800

    Refactor Grass class and remove unused components
    Updated displayAttributes method to be const and removed Water class and main function.

--- cpp/grass.cpp
+++ cpp/grass.cpp
@@ -1,14 +1,10 @@
+// grass.cpp
+#include "metaclass.hpp"
 #include <iostream>
-#include <string>
-
-class Metaclass {
-public:
-    virtual void displayAttributes() = 0;
-};
 
 class Grass : public Metaclass {
 public:
-    void displayAttributes() override {
+    void displayAttributes() const override {
         std::cout << "Grass Attributes:\n"
                   << "Height: 10cm\n"
                   << "Color: Green\n"
@@ -17,26 +13,3 @@ public:
                   << "Interaction: Can be trampled or cut\n";
     }
 };
-
-class Water : public Metaclass {
-public:
-    void displayAttributes() override {
-        std::cout << "Water Attributes:\n"
-                  << "State: Liquid\n"
-                  << "Temperature: 0-100C\n"
-                  << "Flow: Yes\n"
-                  << "Evaporation: Yes\n"
-                  << "Freezing: Yes\n";
-    }
-};
-
-int main() {
-    Grass grass;
-    Water water;
-
-    grass.displayAttributes();
-    water.displayAttributes();
-
-    return 0;
-}
-
