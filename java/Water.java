fb256936dcd3cea6c81fb907bb82721757af6453
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 06:49:21 +0800

    Implement displayAttributes method in Water class

--- java/Water.java
+++ java/Water.java
@@ -1,1 +1,12 @@
+public class Water extends Metaclass {
+    @Override
+    public void displayAttributes() {
+        System.out.println("Water Attributes:");
+        System.out.println("State: Liquid");
+        System.out.println("Temperature: 0-100C");
+        System.out.println("Flow: Yes");
+        System.out.println("Evaporation: Yes");
+        System.out.println("Freezing: Yes");
+    }
+}
 
