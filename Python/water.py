b50cdb80cc41ee247c92dff28d831512c36585a2
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-05 06:51:12 +0800

    Add Water class with display_attributes method

--- Python/water.py
+++ Python/water.py
@@ -1,1 +1,11 @@
+from metaclass import Metaclass
+
+class Water(Metaclass):
+    def display_attributes(self):
+        print("Water Attributes:")
+        print("State: Liquid")
+        print("Temperature: 0-100C")
+        print("Flow: Yes")
+        print("Evaporation: Yes")
+        print("Freezing: Yes")
 
