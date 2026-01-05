dedbb4e207441f466aea21c94aa1576d9d5fb8b5
CeaserZhao <99160798+zbbsdsb@users.noreply.github.com>
2026-01-06 06:52:13 +0800

    Add Grass class implementing Metaclass

--- typescript/grass.ts
+++ typescript/grass.ts
@@ -1,1 +1,15 @@
+import { Metaclass } from "./metaclass";
 
+export class Grass implements Metaclass {
+  readonly type = "grass";
+
+  describe() {
+    return {
+      height: "10cm",
+      color: "green",
+      density: "medium",
+      growthCondition: "humid",
+      interaction: ["trample", "cut"]
+    };
+  }
+}
